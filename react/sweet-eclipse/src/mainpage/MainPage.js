866bde0753747aa4366a43036f46868f86112832
craciun-st <67415094+craciun-st@users.noreply.github.com>
2021-08-19 14:07:28 +0300

    Add MainPage component
    It has a navbar and a footer for now.

---
+++ react/sweet-eclipse/src/mainpage/MainPage.js
@@ -0,0 +1,14 @@
+import './mainpage.css';
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+
+function MainPage(props) {
+    return (
+        <div>
+            <Navbar />
+            <Footer />
+        </div>
+    );
+}
+
+export default MainPage;
