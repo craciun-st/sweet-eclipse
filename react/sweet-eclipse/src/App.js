14a8c8c9b73867430fc7e68d0df46d45988043f8
craciun-st <67415094+craciun-st@users.noreply.github.com>
2021-08-19 14:06:43 +0300

    Add basic Routes in App.js

--- react/sweet-eclipse/src/App.js
+++ react/sweet-eclipse/src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
+import MainPage from './mainpage/MainPage';
+import 'bulma/css/bulma.css';
+
+import {
+    BrowserRouter,
+    Route,
+    Switch
+} from "react-router-dom";
+
+
+
 
 function App() {
   return (
     <div className="App">
-      Learn React
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/"><MainPage /></Route>
+                {/*<Route path="/my-account"><ProfilePage /></Route>*/}
+            </Switch>
+        </BrowserRouter>
     </div>
   );
 }
