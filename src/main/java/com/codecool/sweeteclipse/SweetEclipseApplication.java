b661d0978383ddc95bd1f601d8baaaedf038e078
craciun-st <67415094+craciun-st@users.noreply.github.com>
2021-08-19 16:24:27 +0300

    Add SpringBoot application files
    We will use YAML for setting up properties because it looks nicer

---
+++ src/main/java/com/codecool/sweeteclipse/SweetEclipseApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.sweeteclipse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweetEclipseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SweetEclipseApplication.class, args);
+    }
+
+}
