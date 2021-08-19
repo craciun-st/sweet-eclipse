d95a5285930dd3395ab0fe80ac6fbd621add67c3
craciun-st <67415094+craciun-st@users.noreply.github.com>
2021-08-19 15:18:40 +0300

    Add Signup and Login button components
    Still no 'onClick' logic yet.
    They are separate in the 'authentication' package, as we expect reuse
    outside of the Navbar, as well.

---
+++ react/sweet-eclipse/src/mainpage/authentication/LoginButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function LoginButton(props) {
+    return (
+        <a className="button is-light">
+            Log in
+        </a>
+    );
+}
+
+export default LoginButton;
---
+++ react/sweet-eclipse/src/mainpage/authentication/SignUpButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SignUpButton(props) {
+    return (
+        <a className="button is-primary">
+            <strong>Sign up</strong>
+        </a>
+    );
+}
+
+export default SignUpButton;
