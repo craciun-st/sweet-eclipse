94028d862cf54c6c8b6493c654e16f015688091d
craciun-st <67415094+craciun-st@users.noreply.github.com>
2021-08-19 14:08:24 +0300

    Add Footer component
    Just hardcoded HTML

---
+++ react/sweet-eclipse/src/mainpage/Footer.js
@@ -0,0 +1,14 @@
+import './mainpage.css';
+
+function Footer(props) {
+    return (
+        <div>
+            <footer className="specialFooter container is-fluid">
+                <div>Thank you for visiting us!</div>
+                <div>Sweet Eclipse 2021</div>
+            </footer>
+        </div>
+    );
+}
+
+export default Footer;
