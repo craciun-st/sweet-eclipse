5825a8c8e6daf402fcbbf57e6469f002c4253b98
craciun-st <67415094+craciun-st@users.noreply.github.com>
2021-08-20 13:23:55 +0300

    Update the MainPage in React to show some mock cards
    The width hasn't been sorted out yet, so they are wide as the display.

--- react/sweet-eclipse/src/mainpage/MainPage.js
+++ react/sweet-eclipse/src/mainpage/MainPage.js
@@ -1,11 +1,30 @@
 import './mainpage.css';
 import Footer from "./Footer";
 import Navbar from "./navbar/Navbar";
+import Card from "./card/Card";
 
 function MainPage(props) {
     return (
         <div>
             <Navbar />
+
+
+
+            <Card
+                imgSource="https://via.placeholder.com/120x90/eee?text=Mockup1"
+                title="Mockup project"
+                titleUrlRedirect="#"
+                description="Description of project: Lorem ipsum dolor sit amet"
+            />
+            <Card
+                imgSource="https://via.placeholder.com/120x90/eee?text=Mockup2"
+                title="Mockup project 2"
+                titleUrlRedirect="#"
+                description="Description, etc: Lorem ipsum dolor..."
+            />
+
+
+
             <Footer />
         </div>
     );
