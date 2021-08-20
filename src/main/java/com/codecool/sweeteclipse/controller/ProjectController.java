2cf18df02d119bbc1dc29af4ceca44b3984c0ecb
craciun-st <67415094+craciun-st@users.noreply.github.com>
2021-08-20 12:46:55 +0300

    Add a ProjectController with simple GET all

---
+++ src/main/java/com/codecool/sweeteclipse/controller/ProjectController.java
@@ -0,0 +1,28 @@
+package com.codecool.sweeteclipse.controller;
+
+import com.codecool.sweeteclipse.model.Project;
+import com.codecool.sweeteclipse.repository.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ProjectController {
+
+    private ProjectRepository projectRepo;
+
+    @Autowired
+    public ProjectController(ProjectRepository projectRepo) {
+        this.projectRepo = projectRepo;
+    }
+
+    @Autowired
+
+
+    @GetMapping("/api/projects")
+    public List<Project> getAllProjects() {
+        return projectRepo.findAll();
+    }
+}
