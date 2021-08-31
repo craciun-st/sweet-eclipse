3e97356d531710f437d78be74e0fa6c41906350c
craciun-st <67415094+craciun-st@users.noreply.github.com>
2021-08-31 17:33:01 +0300

    Add CrossOrigin support in the Spring controller
    Reminded by the cors mode on fetch

--- src/main/java/com/codecool/sweeteclipse/controller/ProjectController.java
+++ src/main/java/com/codecool/sweeteclipse/controller/ProjectController.java
@@ -3,11 +3,13 @@ package com.codecool.sweeteclipse.controller;
 import com.codecool.sweeteclipse.model.Project;
 import com.codecool.sweeteclipse.repository.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin("http://localhost:3000")
 @RestController
 public class ProjectController {
 
