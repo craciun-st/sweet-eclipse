277aa2ac497737d426c942edb9da837fc45389c0
craciun-st <67415094+craciun-st@users.noreply.github.com>
2021-08-31 16:24:19 +0300

    Make Card title and description smaller

--- react/sweet-eclipse/src/mainpage/card/CardContent.js
+++ react/sweet-eclipse/src/mainpage/card/CardContent.js
@@ -5,14 +5,14 @@ function CardContent(props) {
         <div className="card-content">
 
             <div className="media-content">
-                <p className="title is-4">
+                <p className="title is-5">
                     <a href={props.urlRedirect? props.urlRedirect : "#"}>
                         {props.title}
                     </a>
                 </p>
 
             </div>
-            <div className="content">
+            <div className="content is-small">
                 {props.description}
             </div>
         </div>
