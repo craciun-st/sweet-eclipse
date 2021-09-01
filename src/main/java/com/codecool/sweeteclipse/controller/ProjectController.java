2df375a79aeec13ad601c2e2922eb4185c411bca
craciun-st <67415094+craciun-st@users.noreply.github.com>
2021-09-01 09:43:23 +0300

    Add /api/project/{id} GET route for individual Project
    It just returns a single Project as JSON, but throws the custom
    exception ObjectIdNotFoundException with 404 Status if ID is not
    present.

--- src/main/java/com/codecool/sweeteclipse/controller/ProjectController.java
+++ src/main/java/com/codecool/sweeteclipse/controller/ProjectController.java
@@ -1,10 +1,12 @@
 package com.codecool.sweeteclipse.controller;
 
+import com.codecool.sweeteclipse.controller.exceptions.ObjectIdNotFoundException;
 import com.codecool.sweeteclipse.model.Project;
 import com.codecool.sweeteclipse.repository.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -26,5 +28,10 @@ public class ProjectController {
     @GetMapping("/api/projects")
     public List<Project> getAllProjects() {
         return projectRepo.findAll();
+    }
+
+    @GetMapping("/api/project/{id}")
+    public Project getProjectById(@PathVariable Long id) {
+        return projectRepo.findById(id).orElseThrow(ObjectIdNotFoundException::new);
     }
 }
