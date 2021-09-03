46ddf1b40b1c85d90f19da313b5747b3eb2f2de8
craciun-st <67415094+craciun-st@users.noreply.github.com>
2021-09-03 15:34:56 +0300

    Update ProjectPage to include new components
    Props for each component are drawn from a unimplemented 'project'
    dictionary/object that we will get from the '/api/project/{id}' route
    
    (This is why we need the {id} variable from useParams() of
    'react-router-dom')

--- react/sweet-eclipse/src/projectpage/ProjectPage.js
+++ react/sweet-eclipse/src/projectpage/ProjectPage.js
@@ -4,14 +4,37 @@ import React, {useState, useEffect} from 'react';
 
 import {useParams} from 'react-router-dom';
 import Navbar from "../navbar/Navbar";
+import BigImage from "./BigImage";
+import ProgressBar from "./ProgressBar";
+import FancyButton from "./FancyButton";
 function ProjectPage(props) {
     let {id} = useParams();
 
+    percentValue = project.fundingGoal ? Math.trunc(project.currentFunds/project.fundingGoal * 100) : 0;
 
     return (
         <div>
             <Navbar />
 
+            <BigImage imgSource={project.images[0].uri}/>
+            <ProgressBar
+                currentFunds={project.currentFunds}
+                fundingGoal={project.fundingGoal}
+                nrDonors={project.nrDonors}
+                percentValue={percentValue}
+            />
+            <div>
+                <FancyButton>Donate!</FancyButton>
+                <FancyButton>Share</FancyButton>
+            </div>
+            <br />
+            <br />
+            <div className="title is-3">
+                {project.title}
+            </div>
+            <div className="content is-small">
+                {project.description}
+            </div>
         </div>
     );
 }
