1f3d5b47098ee84c9c70e55109cf23b252de52c5
craciun-st <67415094+craciun-st@users.noreply.github.com>
2021-09-03 15:29:45 +0300

    Revert DB initialization
    Required, since we changed the schema by altering model;
    'create' mode for 'hibernate.ddl-auto' should suffice from
    now on for demo purposes (as opposed to reverting back and forth between
    'create' / 'update')

--- src/main/java/com/codecool/sweeteclipse/init/DbInit.java
+++ src/main/java/com/codecool/sweeteclipse/init/DbInit.java
@@ -28,35 +28,42 @@ public class DbInit implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-//        ImageData image1 = new ImageData("/image_0001.png", null);
-//        ImageData image2 = new ImageData("/image_0002.png", null);
-//        ImageData image3 = new ImageData("/image_0003.png", null);
-//        ProjectTag tag1 = new ProjectTag("pets");
-//        ProjectTag tag2 = new ProjectTag("environment");
-//        ProjectTag tag3 = new ProjectTag("community");
-//        Project strayDogProject = new Project(
-//                "Save the stray dogs!",
-//                "Collaborate to find a shelter for these misunderstood beings",
-//                1000
-//        );
-//        Project riverCleanupProject = new Project(
-//                "Help fund the biggest river cleanup effort!",
-//                "The river X has too many junk floating in it! We need your help to finance a cleanup operation.",
-//                2000
-//        );
-//
-//
-//        strayDogProject.addImage(image1);
-//        strayDogProject.addImage(image2);
-//        riverCleanupProject.addImage(image3);
-//
-//        strayDogProject.addTag(tag1);
-//        riverCleanupProject.addTag(tag2);
-//        riverCleanupProject.addTag(tag3);
-//
-//        projectRepo.saveAll(List.of(strayDogProject, riverCleanupProject));
-//        imageRepo.saveAll(List.of(image1, image2, image3));
-//        tagRepo.saveAll(List.of(tag1, tag2));
+        ImageData image1 = new ImageData("/image_0001.png", null);
+        ImageData image2 = new ImageData("/image_0002.png", null);
+        ImageData image3 = new ImageData("/image_0003.png", null);
+        ProjectTag tag1 = new ProjectTag("pets");
+        ProjectTag tag2 = new ProjectTag("environment");
+        ProjectTag tag3 = new ProjectTag("community");
+        Project strayDogProject = new Project(
+                "Save the stray dogs!",
+                "Collaborate to find a shelter for these misunderstood beings",
+                1000
+        );
+        Project riverCleanupProject = new Project(
+                "Help fund the biggest river cleanup effort!",
+                "The river X has too many junk floating in it! We need your help to finance a cleanup operation.",
+                2000
+        );
+
+
+        strayDogProject.addImage(image1);
+        strayDogProject.addImage(image2);
+        riverCleanupProject.addImage(image3);
+
+        strayDogProject.addTag(tag1);
+        riverCleanupProject.addTag(tag2);
+        riverCleanupProject.addTag(tag3);
+
+
+        strayDogProject.setCurrentFunds(45.75);
+        riverCleanupProject.setCurrentFunds(507.48);
+
+        strayDogProject.setNrDonors(8);
+        riverCleanupProject.setNrDonors(89);
+
+        projectRepo.saveAll(List.of(strayDogProject, riverCleanupProject));
+        imageRepo.saveAll(List.of(image1, image2, image3));
+        tagRepo.saveAll(List.of(tag1, tag2));
 
     }
 }
