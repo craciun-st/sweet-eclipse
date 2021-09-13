cdc529a437007440152a2699d373eb9efe5c946c
craciun-st <67415094+craciun-st@users.noreply.github.com>
2021-09-13 13:18:41 +0300

    Add a WebSecurityConfig
    This is basically to enable the DaoAuthenticationProvider which in
    turn uses a PasswordEncoder to simplify password handling
    (technically, we just need to encode when the password is set;
    password decoding/matching is done via the Provider)
    We use BCrypt as the cryptographic algorithm.
    
    Of course, we will later use the WebSecurityConfig.configure(http)
    method to require users to be authenticated on some endpoints.

---
+++ src/main/java/com/codecool/sweeteclipse/config/WebSecurityConfig.java
@@ -0,0 +1,73 @@
+package com.codecool.sweeteclipse.config;
+
+import com.codecool.sweeteclipse.service.UserDetailsServiceImplemented;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.security.SecureRandom;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private UserDetailsServiceImplemented userDetailsService;
+
+    @Autowired
+    public WebSecurityConfig(UserDetailsServiceImplemented userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(daoAuthenticationProvider());
+    }
+
+
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        int strength = 10;  // 2^strength iterations (supposedly, the nr. of XOR filters in the algorithm)
+                            // (10 is sufficiently hard for current hardware to brute-force)
+
+        return new BCryptPasswordEncoder(strength, new SecureRandom()); // use a cryptographically secure random nr.
+                                                                        // as the salt
+
+            // for testing purposes (use no encoding)
+        // return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public AuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder());
+        provider.setUserDetailsService(this.userDetailsService);
+        return provider;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.csrf().disable()
+                .authorizeRequests()
+//                .antMatchers("/login").permitAll()
+                .antMatchers("/**").permitAll()
+                .anyRequest().permitAll()
+                .and()
+                .exceptionHandling();
+
+
+    }
+
+
+
+}
