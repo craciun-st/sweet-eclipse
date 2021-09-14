90f40ad3e75e9f2b14fda437e2ba8a70d63691ea
craciun-st <67415094+craciun-st@users.noreply.github.com>
2021-09-14 13:12:21 +0300

    Add a BasicAuthEntryPoint
    This is just a bean which handles what happens when a User fails
    a BasicAuth header challenge.
    
    We do not have any special logic here except for setting the
    realm name for the 'WWW-Authenticate' header in the response.

---
+++ src/main/java/com/codecool/sweeteclipse/controller/BasicAuthEntryPoint.java
@@ -0,0 +1,31 @@
+package com.codecool.sweeteclipse.controller;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class BasicAuthEntryPoint extends BasicAuthenticationEntryPoint {
+
+    @Override
+    public void afterPropertiesSet() {
+        setRealmName("Sweet Eclipse");
+        super.afterPropertiesSet();
+    }
+
+    @Override
+    public void commence(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            AuthenticationException authException
+    ) throws IOException {
+        // the super sets the response status to HTTP 401 (we have an authException and auth has failed)
+        // it also sets the response header's 'WWW-Authenticate' field to our realm name
+        // If we want something more complex, we could override this method in earnest
+        super.commence(request, response, authException);
+    }
+}
