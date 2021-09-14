413969d4a14f1e14bda7d6ec341fb7e9bfb09d5f
craciun-st <67415094+craciun-st@users.noreply.github.com>
2021-09-14 12:52:29 +0300

    Minor import cleanup
    Also removed an accidental Autowired annotation in
    ProjectController

--- src/main/java/com/codecool/sweeteclipse/repository/DonationRepository.java
+++ src/main/java/com/codecool/sweeteclipse/repository/DonationRepository.java
@@ -6,8 +6,6 @@ import com.codecool.sweeteclipse.model.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface DonationRepository extends JpaRepository<Donation, Long> {
 
