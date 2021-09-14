4b9ae4e56797b5ad8bfa765079cca3b4cdef1ccf
craciun-st <67415094+craciun-st@users.noreply.github.com>
2021-09-14 13:08:58 +0300

    Add a UserController
    It has the POST '/api/signup' route where registering will take
    place.
    It also has the GET '/api/user/{id}' route where may access public
    data about a single user.
    
    It uses Spring HATEOAS Link-builder to generate the Location
    header in the 201 status response when successfully creating a
    new User (that is, it automatically generates a URI at which the
    new User can be accessed).
    That same User's PublicData is returned in the response
    body.
    
    It also handles the validation errors on the constraints on the
    SignUpRequest object and returns a 400 status with a
    {incorrectField}: {errorMessage} dictionary as body.
    
    It also handles the UserAlreadyExistException in a similar
    fashion, but returns a 403 status.

---
+++ src/main/java/com/codecool/sweeteclipse/controller/UserController.java
@@ -0,0 +1,91 @@
+package com.codecool.sweeteclipse.controller;
+
+import com.codecool.sweeteclipse.controller.exceptions.ObjectIdNotFoundException;
+import com.codecool.sweeteclipse.model.user.PublicUserData;
+import com.codecool.sweeteclipse.model.user.User;
+import com.codecool.sweeteclipse.model.authentication.SignUpRequest;
+import com.codecool.sweeteclipse.repository.UserRepository;
+import com.codecool.sweeteclipse.service.UserManagementService;
+import com.codecool.sweeteclipse.service.exceptions.UserAlreadyExistException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+
+@CrossOrigin("http://localhost:3000")
+@RestController
+public class UserController {
+
+    Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    private UserRepository userRepo;
+    private UserManagementService userManagementService;
+
+    @Autowired
+    public UserController(UserRepository userRepo, UserManagementService userManagementService) {
+        this.userRepo = userRepo;
+        this.userManagementService = userManagementService;
+    }
+
+
+
+
+    @PostMapping("/api/signup")
+    public ResponseEntity<PublicUserData> addUser(
+            @Valid @RequestBody SignUpRequest userInfo
+    ) throws UserAlreadyExistException {
+        User createdUser = userManagementService.registerNewUserAccount(userInfo);
+
+        Long createdUserId = createdUser.getId();
+        URI createdUserUri = linkTo(
+                methodOn(UserController.class).getPublicUserInfo(createdUserId)
+        ).toUri();
+        PublicUserData createdUserPublicData = userManagementService.mapUserToPublicData(createdUser);
+
+        return ResponseEntity.created(createdUserUri).body(createdUserPublicData);
+    }
+
+
+
+    @GetMapping("/api/user/{id}")
+    public ResponseEntity<PublicUserData> getPublicUserInfo(@PathVariable Long id) {
+        User foundUser = userRepo.findUserById(id)
+                .orElseThrow(ObjectIdNotFoundException::new);
+        PublicUserData foundUserPublicData = userManagementService.mapUserToPublicData(foundUser);
+        return ResponseEntity.ok(foundUserPublicData);
+    }
+
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    @ExceptionHandler(UserAlreadyExistException.class)
+    public Map<String, String> handleUserAlreadyExistsException(UserAlreadyExistException ex) {
+        Map<String, String> response = new HashMap<>();
+        response.put("username", "Username already exists!");
+        return response;
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach(
+                (error) -> {
+                    String fieldName = ((FieldError) error).getField();
+                    String errorMessage = error.getDefaultMessage();
+                    errors.put(fieldName, errorMessage);
+                }
+        );
+        return errors;
+    }
+}
