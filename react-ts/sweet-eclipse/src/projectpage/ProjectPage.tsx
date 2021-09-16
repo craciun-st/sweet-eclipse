566163f0d4cc928bf93eb65cb4eba939b0cda453
craciun-st <67415094+craciun-st@users.noreply.github.com>
2021-09-16 17:15:06 +0300

    Treat the case when given a non-existent Project id
    In such a case we will get a 404 from the server
    and we set the state to the initialProject (which had a MISSING_INFO
    status)
    
    This status can be used as a condition to not display what is inside
    .projectPageContainer

--- react-ts/sweet-eclipse/src/projectpage/ProjectPage.tsx
+++ react-ts/sweet-eclipse/src/projectpage/ProjectPage.tsx
@@ -12,11 +12,14 @@ import {Project} from "../ts-declarations/Project";
 import SocialMediaContainer from "./SocialMediaContainer";
 import ShareButton from "./buttons/ShareButton";
 import TagContainer from "./tags/TagContainer";
+import {useHistory} from "react-router-dom";
 
 export const DEFAULT_PP_ELEMENT_WIDTH = "55vw";
 
 function ProjectPage(props: any) {
     let {id} = useParams<{ id: string }>();
+
+    const browserHistory = useHistory();
 
     const initialProject: Project = {
         currentFunds: 0,
@@ -52,7 +55,11 @@ function ProjectPage(props: any) {
             doGet(
                 'http://localhost:8080/api/project/' + id,
                 (data: Project) => {
-                    if (data) {setProject(data)}
+                    if (data.id && data.title) {
+                        setProject(data)
+                    } else {
+                        setProject(initialProject)
+                    }
                     console.log(data);
                 }
             )
@@ -69,40 +76,44 @@ function ProjectPage(props: any) {
     return (
         <div>
             <Navbar/>
-            <div className={"projectPageContainer"}>
+            {project.status === "MISSING_INFO" ? "No such project!" : (
+                <div className={"projectPageContainer"}>
 
-                <BigImage imgSource={project.images ? project.images[0].uri : ""}/>
-                <ProgressBar
-                    currentFunds={project.currentFunds}
-                    fundingGoal={project.fundingGoal}
-                    nrDonors={project.nrDonors}
-                    percentValue={percentValue}
-                />
-                <SocialMediaContainer>
-                    <TagContainer tags={ project.tags ? project.tags : [] }/>
-                    <ShareButton>Share</ShareButton>
-                </SocialMediaContainer>
-                <br/>
-
-                <div style={{width: DEFAULT_PP_ELEMENT_WIDTH}}>
-                    <FancyButton
-                        classAddons={" is-link is-large is-fullwidth"}
-                        icon={"fas fa-piggy-bank"}
-                    >
-                        Donate!
-                    </FancyButton>
-
+                    <BigImage imgSource={project.images ? project.images[0].uri : ""}/>
+                    <ProgressBar
+                        currentFunds={project.currentFunds}
+                        fundingGoal={project.fundingGoal}
+                        nrDonors={project.nrDonors}
+                        percentValue={percentValue}
+                    />
+                    <SocialMediaContainer>
+                        <TagContainer tags={project.tags ? project.tags : []}/>
+                        <ShareButton>Share</ShareButton>
+                    </SocialMediaContainer>
                     <br/>
-                    <br/>
-                    <div className="title is-3">
-                        {project.title}
+
+                    <div style={{width: DEFAULT_PP_ELEMENT_WIDTH}}>
+                        <FancyButton
+                            classAddons={" is-link is-large is-fullwidth"}
+                            icon={"fas fa-piggy-bank"}
+                        >
+                            Donate!
+                        </FancyButton>
+
+                        <br/>
+                        <br/>
+                        <div className="title is-3">
+                            {project.title}
+                        </div>
+                        <div className="content is-small">
+                            {project.description}
+                        </div>
                     </div>
-                    <div className="content is-small">
-                        {project.description}
-                    </div>
+
                 </div>
-            </div>
+            )}
         </div>
+
     );
 }
 
