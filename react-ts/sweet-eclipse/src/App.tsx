dc2c19c26136dee59dafa556108e88a70b6c5410
craciun-st <67415094+craciun-st@users.noreply.github.com>
2021-09-17 14:06:57 +0300

    Add isLoggedInAtom to track log status
    The log-status is the prototypical example of a global state
    variable, a.k.a an atom from jotai
    
    It is exported from the parent App component

--- react-ts/sweet-eclipse/src/App.tsx
+++ react-ts/sweet-eclipse/src/App.tsx
@@ -4,6 +4,9 @@ import 'bulma/css/bulma.css';
 
 import {BrowserRouter, Route, Switch} from "react-router-dom";
 import ProjectPage from "./projectpage/ProjectPage";
+import {atom} from "jotai";
+
+export const isLoggedInAtom = atom(false)
 
 
 function App() {
