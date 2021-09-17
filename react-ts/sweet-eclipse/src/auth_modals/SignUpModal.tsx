7bdd27626e256749b92e2ef3678733f23be6677a
craciun-st <67415094+craciun-st@users.noreply.github.com>
2021-09-17 04:59:14 +0300

    Add the SignUpModal
    This will require to introduce an onSuccessfulCreate prop to
    SignUpForm, which is a function (data: {}) => void
    
    This is because we don't want to close the modal within the SignUpForm
    itself, rather we do it here.

---
+++ react-ts/sweet-eclipse/src/auth_modals/SignUpModal.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import {atom, useAtom} from "jotai";
+import SignUpForm from "./SignUpForm";
+import BackToMainButton from "../general_buttons/BackToMainButton";
+
+export const isSignUpModalActive = atom(false)
+
+function SignUpModal(props: any) {
+    const [isActive, setIsActive] = useAtom(isSignUpModalActive)
+
+
+    function closeModal() {
+        setIsActive(false)
+    }
+
+    function handleSubmitSuccess(data: {}) {
+        localStorage.setItem('sweetEclipse', JSON.stringify(data));
+        closeModal();
+    }
+
+    return (
+        <div className={"modal" + (isActive ? " is-active" : "")}>
+            <div className="modal-background"/>
+            <div className="modal-card">
+                <header className="modal-card-head">
+                    <p className="modal-card-title">Sign Up</p>
+                    <button className="delete" aria-label="close" onClick={closeModal}/>
+                </header>
+                <section className="modal-card-body">
+                    <SignUpForm onSuccessfulCreate={(data => handleSubmitSuccess(data))}/>
+                </section>
+                <footer className="modal-card-foot">
+                    {/*<button className="button is-success">Save changes</button>*/}
+                    {/*<button className="button" onClick={closeModal}>Cancel</button>*/}
+                    <BackToMainButton classNameAddon={" is-outlined"} onClick={closeModal}/>
+                </footer>
+            </div>
+        </div>
+    );
+}
+
+export default SignUpModal;
