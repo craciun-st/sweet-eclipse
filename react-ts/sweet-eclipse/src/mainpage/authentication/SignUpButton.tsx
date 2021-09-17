38e5b797887b879b6e16cdc5541cee8a7c0c30cb
craciun-st <67415094+craciun-st@users.noreply.github.com>
2021-09-17 04:48:45 +0300

    Update SignUpButton with new code

--- react-ts/sweet-eclipse/src/mainpage/authentication/SignUpButton.tsx
+++ react-ts/sweet-eclipse/src/mainpage/authentication/SignUpButton.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import SignUpModal, {isSignUpModalActive} from "../../auth_modals/SignUpModal";
+import {useAtom} from "jotai";
+
+
 
 function SignUpButton(props: any) {
+
+    const [shouldDisplayModal, setShouldDisplayModal] = useAtom(isSignUpModalActive)
+
+    function launchSignUpModal() {
+        setShouldDisplayModal(true);
+    }
     return (
-        <a className="button is-primary" href={"/signup"}>
-            <strong>Sign up</strong>
-        </a>
+        <div>
+            <button className="button is-primary is-light" onClick={launchSignUpModal}>
+                <strong>{props.children}</strong>
+            </button>
+            <SignUpModal isActive={shouldDisplayModal}/>
+        </div>
     );
+
+
 }
 
 export default SignUpButton;
