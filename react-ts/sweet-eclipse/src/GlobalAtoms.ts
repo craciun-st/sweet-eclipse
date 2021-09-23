2f948bca1af48a6259721d71e72d0a4ef3e4ae65
craciun-st <67415094+craciun-st@users.noreply.github.com>
2021-09-23 08:31:21 +0300

    Add GlobalAtoms for data on '/donate/' page
    The idea is that we don't want '/donate/{id}' pages all over the place,
    but we also need to link some information here from the departed
    '/project/{id}' page
    
    We also don't want to do a refetch of data
    on the '/api/project/{id}' route, since we already have everything
    we need stored in the state of the departed page.

--- react-ts/sweet-eclipse/src/GlobalAtoms.ts
+++ react-ts/sweet-eclipse/src/GlobalAtoms.ts
@@ -6,3 +6,8 @@ export const isLoggedInAtom = atom(false)
 export const isDonateModalActive = atom(false)
 export const isSignUpModalActive = atom(false)
 export const isLoginModalActive = atom(false)
+
+// Donation data
+export const titleForDonationIntent = atom("")
+export const imageUriForDonationIntent = atom("https://via.placeholder.com/120x90/eee?text=missing")
+export const idForDonationIntent = atom(-1)
