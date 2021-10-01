b4825e0fc3abfc087331cf75c51195fcc7395ff2
craciun-st <67415094+craciun-st@users.noreply.github.com>
2021-10-01 16:04:49 +0300

    Fix missing @Component on ServerInit
    This will not register the server's apiKey
    (namely, the secret key: 'sk_...')
    if it is not loaded in context

--- src/main/java/com/codecool/sweeteclipse/init/ServerInit.java
+++ src/main/java/com/codecool/sweeteclipse/init/ServerInit.java
@@ -3,7 +3,9 @@ package com.codecool.sweeteclipse.init;
 import com.stripe.Stripe;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ServerInit implements CommandLineRunner {
     @Value("${stripe.apiKey}")
     String stripeSecret;
