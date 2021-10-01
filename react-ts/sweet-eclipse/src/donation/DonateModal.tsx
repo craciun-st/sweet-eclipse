42fe95917cd58937726b0255b8501a086e6fda35
craciun-st <67415094+craciun-st@users.noreply.github.com>
2021-10-01 15:32:51 +0300

    Add the DonateModal
    Now, the amount is inputted in a modal, rather than on the DonatePage
    That is where the card/banking details will be inputted.

---
+++ react-ts/sweet-eclipse/src/donation/DonateModal.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import {useAtom} from "jotai";
+import {
+    idForDonationIntent,
+    imageUriForDonationIntent,
+    isDonateModalActive,
+    titleForDonationIntent
+} from "../GlobalAtoms";
+import DonateForm from "./DonateForm";
+import {useHistory} from "react-router-dom";
+import BackToMainButton from "../general_buttons/BackToMainButton";
+
+function DonateModal(props: any) {
+    const [isActive, setIsActive] = useAtom(isDonateModalActive)
+    const [titleForProject, setTitleForProject] = useAtom(titleForDonationIntent);
+    const [imageUriForProject, setImageUriForProject] = useAtom(imageUriForDonationIntent);
+    const [idForProject, setIdForProject] = useAtom(idForDonationIntent);
+    const browserHistory = useHistory();
+
+    function closeModal() {
+        setIsActive(false)
+    }
+
+    function handleSubmitSuccess() {
+
+        closeModal();
+        browserHistory.push("/donate")
+    }
+
+    return (
+        <div className={"modal" + (isActive ? " is-active" : "")}>
+            <div className="modal-background"/>
+            <div className="modal-card">
+                <header className="modal-card-head">
+                    <p className="modal-card-title">Login</p>
+                    <button className="delete" aria-label="close" onClick={closeModal}/>
+                </header>
+                <section className="modal-card-body">
+                    <DonateForm onSuccessfulCreate={handleSubmitSuccess}/>
+                </section>
+                <footer className="modal-card-foot">
+                    <BackToMainButton onClick={(event) => {
+                        setImageUriForProject("");
+                        setTitleForProject("");
+                        setIdForProject(-1);
+                        closeModal();
+                    }} classNameAddon={" is-outlined block"} />
+                </footer>
+            </div>
+        </div>
+    );
+}
+
+export default DonateModal;
