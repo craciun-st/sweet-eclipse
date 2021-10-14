080ba35805567523fe77c3bbaed012c24bf37f1e
craciun-st <67415094+craciun-st@users.noreply.github.com>
2021-10-14 16:09:49 +0300

    Update ImageServiceFacade with default methods
    These throw ImproperFileException for some edge cases
    (empty file, MimeType is not "image/* " )
    
    This refactor led to the better method
    getUriStringAfterSavingOrElseThrow()

--- src/main/java/com/codecool/sweeteclipse/service/ImageServiceFacade.java
+++ src/main/java/com/codecool/sweeteclipse/service/ImageServiceFacade.java
@@ -1,12 +1,48 @@
 package com.codecool.sweeteclipse.service;
 
+import com.codecool.sweeteclipse.model.ImageData;
+import com.codecool.sweeteclipse.service.exceptions.ImproperFileException;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 
 public interface ImageServiceFacade {
 
+
     String getUriAfterSavingProperFile(MultipartFile properImageFile, String forProjectName) throws IOException;
 
+    String getUriStringAfterSavingOrElseThrow(
+            MultipartFile multipartFile,
+            String forProject
+    ) throws ImproperFileException, IOException;
+
     byte[] getRawImageById(Long id) throws IOException;
+
+
+
+
+
+    default void throwIfFileIsEmpty(MultipartFile multipartFile) throws ImproperFileException {
+        if (multipartFile.isEmpty()) {
+            throw new ImproperFileException("Failed to upload empty file");
+        }
+    }
+
+    default void throwIfNotAnImageType(MultipartFile multipartFile) throws ImproperFileException {
+
+        if (!IMAGE_MIME_TYPES.contains( multipartFile.getContentType() )) {
+            throw new ImproperFileException("Will not upload a file without a proper image MIME-type");
+        }
+    }
+
+    List<String> IMAGE_MIME_TYPES = List.of(
+            "image/bmp",
+            "image/jpeg",
+            "image/png",
+            "image/svg+xml",
+            "image/tiff",
+            "image/webp"
+    );
+
 }
