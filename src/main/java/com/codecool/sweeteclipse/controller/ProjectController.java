b7a1572f5263c4c35a3666321d753444360325c7
craciun-st <67415094+craciun-st@users.noreply.github.com>
2021-10-14 18:57:49 +0300

    Add /api/dev_test/images route to test basic upload
    It throws ImproperFileException, but also some controller-specific
    exceptions.
    Unlike ImproperFileException which is a client's fault, these would be
    server-side errors.
    GenericInternalServerError corresponds to a HTTP code 500;
    ThirdPartyServiceException corresponds to a HTTP code 503.

--- src/main/java/com/codecool/sweeteclipse/controller/ProjectController.java
+++ src/main/java/com/codecool/sweeteclipse/controller/ProjectController.java
@@ -15,12 +15,19 @@ import java.util.List;
 @RestController
 public class ProjectController {
 
+    Logger logger = LoggerFactory.getLogger(ProjectController.class);
+
     private ProjectRepository projectRepo;
 
+    private ImageServiceFacade imageService;
+
     @Autowired
-    public ProjectController(ProjectRepository projectRepo) {
+    public ProjectController(ProjectRepository projectRepo, ImageService imageService) {
         this.projectRepo = projectRepo;
+        this.imageService = imageService;
     }
+
+
 
 
 
@@ -32,5 +39,39 @@ public class ProjectController {
     @GetMapping("/api/project/{id}")
     public Project getProjectById(@PathVariable Long id) {
         return projectRepo.findById(id).orElseThrow(ObjectIdNotFoundException::new);
+    }
+    /**
+     * Test endpoint for checking AWS upload
+     * (Note: Images are tightly coupled to projects, so no need to have a separate controller)
+     *
+     * @param multipartFile a (image) file from a multipart form data
+     * @return (as JSON) a map with key 'uri' and value the AWS S3 URL of the uploaded file (if successful)
+     * @throws ThirdPartyServiceException
+     * @throws ImproperFileException
+     * @throws GenericInternalServerError
+     */
+    @PostMapping(
+            value = "/api/dev_test/images",
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<Map<String, String>> uploadImage(@RequestParam("file") MultipartFile multipartFile)
+            throws ThirdPartyServiceException, ImproperFileException, GenericInternalServerError
+    {
+        String uriString = "";
+        try {
+            uriString = imageService.getUriStringAfterSavingOrElseThrow(multipartFile, "test_upload");
+        } catch (TempStorageException exception) {
+            logger.error(exception.getMessage());
+            throw new GenericInternalServerError();
+        } catch (IOException exception) {
+            logger.warn(exception.getMessage());
+            throw new ThirdPartyServiceException();
+        }
+
+        URI createdImageLink = URI.create(uriString);
+        Map<String, String> response = new HashMap<>();
+        response.put("uri", createdImageLink.toASCIIString());
+        return ResponseEntity.created(createdImageLink).body(response);
     }
 }
